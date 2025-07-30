b56ed4e9003bc08180eb2dca7e848ce380c5f589
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-07-30 20:11:08 +0000

    added routes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# Keep your existing BookList view if you want both options
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Add the new ViewSet
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
