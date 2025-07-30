d9b3935d478ef8e552e26e8c0dba72ec02e444ef
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-07-30 20:25:36 +0000

    add urls

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-# Keep your existing BookList view if you want both options
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
 
-# Add the new ViewSet
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
