8fff3a25d21d44b681c6c8ad6b098290b104d309
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-23 21:09:27 +0000

    added templates

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -29,8 +29,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
