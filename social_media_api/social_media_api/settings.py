97aa776558c690233cb4d75481127eadc6957db2
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 20:30:13 +0000

    added posts app

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts', 
+    'posts', 
     'rest_framework', 
     'rest_framework.authtoken', 
 ]
@@ -125,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.User' 
+# AUTH_USER_MODEL = 'accounts.User' 
 
 
 REST_FRAMEWORK = {
