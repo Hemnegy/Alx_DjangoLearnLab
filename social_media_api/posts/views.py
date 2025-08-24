7ad1d9428567044d4499fbb9232cc6029f0c4418
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 20:53:23 +0000

    updated the accounts.views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -20,3 +21,22 @@ class CommentViewSet(viewsets.ModelViewSet):  # <-- "viewsets.ModelViewSet"
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(generics.GenericAPIView):
+    """
+    Returns posts from users that the authenticated user follows.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Get all the users the current user is following
+        following_users = request.user.following.all()  # <-- "following.all()"
+
+        # Get posts authored by those users
+        posts = Post.objects.filter(author__in=following_users).order_by("-created_at")  
+        # <-- "Post.objects.filter(author__in=following_users).order_by"
+
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
