1852a896b103d199961bd7d7c762a9b5970bc8e5
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 16:09:43 +0000

    added routes to urls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,25 +2,23 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # Use get_user_model().objects.create_user directly
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture')
         )
-        Token.objects.create(user=user)  # create auth token automatically
+        Token.objects.create(user=user)  # automatically create auth token
         return user
 
 
@@ -31,5 +29,5 @@ class UserLoginSerializer(serializers.Serializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
