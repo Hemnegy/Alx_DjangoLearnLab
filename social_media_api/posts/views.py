f5d5d75e2bbeefb42d766e1b3420d9bef639b8ee
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 20:43:02 +0000

    added views to posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):  # <-- "viewsets.ModelViewSet"
+    queryset = Post.objects.all().order_by("-created_at")  # <-- "Post.objects.all()"
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):  # <-- "viewsets.ModelViewSet"
+    queryset = Comment.objects.all().order_by("-created_at")  # <-- "Comment.objects.all()"
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
